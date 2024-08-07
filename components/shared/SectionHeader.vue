3d5f417f64d922250e17905a72ecd5e707605391
doceee <piotr96X@wp.pl>
2024-08-07 15:38:50 +0200

    update section header component

--- components/shared/SectionHeader.vue
+++ components/shared/SectionHeader.vue
@@ -1,5 +1,7 @@
 <template>
-    <h2 class="mb-[24px] text-[42px] font-bold leading-[52px]">
-        <slot />
-    </h2>
+    <header class="mb-[24px]">
+        <h2 class="text-[42px] font-bold leading-[52px]">
+            <slot />
+        </h2>
+    </header>
 </template>
